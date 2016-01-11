c94d5ea8d57bbbd8372f31f8f1ab0efad4b02d73
Easterling <windows146@users.noreply.github.com>
2016-01-11 04:08:22 -0500

    PlayScreen

---
+++ core/src/game/pantyteam/PlayScreen.java
@@ -0,0 +1,62 @@
+package game.pantyteam;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class PlayScreen implements Screen {
+
+	private SpriteBatch batch;
+	Texture img;
+
+	public PlayScreen(SpriteBatch batch) {
+		this.batch = batch;
+	}
+
+	@Override
+	public void show() {
+		img = new Texture("badlogic.jpg");
+	}
+
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
