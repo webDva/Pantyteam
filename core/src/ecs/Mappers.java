d8edb83c140dc0b5353bb69c8893f6bc15ce1747
Easterling <windows146@users.noreply.github.com>
2016-01-11 08:38:48 -0500

    added to mappers

--- core/src/ecs/Mappers.java
+++ core/src/ecs/Mappers.java
@@ -3,7 +3,9 @@ package ecs;
 import com.badlogic.ashley.core.ComponentMapper;
 
 import ecs.components.PhysicsComponent;
+import ecs.components.PositionComponent;
 
 public class Mappers {
 	public static final ComponentMapper<PhysicsComponent> physics = ComponentMapper.getFor(PhysicsComponent.class);
+	public static final ComponentMapper<PositionComponent> position = ComponentMapper.getFor(PositionComponent.class);
 }
